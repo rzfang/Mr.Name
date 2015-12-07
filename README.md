c63f6367dc395d7f5be5815143207f6555e16c71
RZ Fang <skywine13@gmail.com>
2015-12-07 18:06:19 +0800

    initail commit.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Mr.Name
 Mr.Name is a GUI files rename tool.
+
+made by python 3, Gtk+ 3.
---
+++ UTL/Mr.Name.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Note:
+# copy this script into nautilus script folder.
+
+# AppPth=$HOME"/Tool/"; # 'AppPth' = Application Path.
+# AppNm="Mr.Name.py"; # 'AppNm' = Application Name.
+
+# cd $AppPth;
+
+# if [ -z "$NEMO_SCRIPT_CURRENT_URI" ];
+# then
+#   ./$AppNm;
+# else
+#   ./$AppNm $NEMO_SCRIPT_CURRENT_URI;
+# fi;
